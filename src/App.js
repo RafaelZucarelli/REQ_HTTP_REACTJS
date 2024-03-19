8813770ca0b31824fb808449aaec991561e902c9
RafaelZucarelli <zucarelli83@gmail.com>
2024-03-19 16:41:35 -0300

    adding JSON Server data

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
 
   const [products, setProducts] = useState([]);
 
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+
   //resgatando dados - da onde? = url base "http://localhost:3000/products"
   useEffect(() => {
     const fetchData = async () => {
@@ -22,14 +26,56 @@ function App() {
     fetchData();
   }, []);
 
-  console.log(products);
+  // add de produtos
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const product = {
+      name,
+      price,
+    }
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+  };
 
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
       {products.map((product) => (
         <li key={product.id}>{product.name} - {product.price}</li>
-      ))};
+      ))}
+      <div className='add-product'>
+        <form onSubmit={handleSubmit}>
+            <label>
+              Nome:
+              <input
+              type='text'
+              value={name}
+              name='name'
+              onChange={(e) => setName(e.target.value)}
+              >
+              </input>
+            </label>
+
+            <label>
+              Preço:
+              <input
+              type='number'
+              value={price}
+              name='price'
+              onChange={(e) => setPrice(e.target.value)}
+              />
+              
+            </label >
+           < input type='submit' value="Criar"/>
+        </form>
+      </div>
     </div>
   );
 }
